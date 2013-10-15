9c2c53ab8e9389067206beb733ca8a3e2e6129bb
jonah <jonahkjala@gmail.com>
2013-10-16 03:07:40 +0800

    smslib config again

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -4,14 +4,14 @@ beans = {
 	
 	switch(Environment.current) {
 		case Environment.PRODUCTION:
-		smsGateway(org.klinikav2.sms.ModemSmsGateway) {bean->
+		smsGateway(org.itech.klinikav2.sms.ModemSmsGateway) {bean->
 		bean.initMethod = 'init'
 		bean.destroyMethod = 'destroy'
-		modemPort=application.config.org.klinikav2.sms.modemPort
+		modemPort=application.config.org.itech.klinikav2.sms.modemPort
 		}
 		break
 		case Environment.DEVELOPMENT:
-		smsGateway(org.klinikav2.sms.MockSmsGateway)
+		smsGateway(org.itech.klinikav2.sms.MockSmsGateway)
 		break
 		}
 //	itemNotifier(org.itech.klinikav2.domain.ItemNotifier){
